e52c5ab812b5880066a5874478ca03da87474976
b-forster <brianna.forster@gmail.com>
2019-03-07 15:46:12 -0800

    Fetch list of articles from given category with mediawiki API

--- app/controllers/category_search_controller.rb
+++ app/controllers/category_search_controller.rb
@@ -1,3 +1,6 @@
+require 'uri'
+require 'net/http'
+
 class CategorySearchController < ApplicationController
   def index
     render 'index'
@@ -5,5 +8,17 @@ class CategorySearchController < ApplicationController
 
   def create
     category = params[:category]
+    url = URI("https://en.wikipedia.org/w/api.php?action=query&list=categorymembers&cmtitle=Category%3A#{params[:category]}&format=json")
+
+    http = Net::HTTP.new(url.host, url.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    request = Net::HTTP::Get.new(url)
+
+    response = http.request(request)
+    category_members = JSON.parse(response.read_body)['query']['categorymembers']
   end
+
+
 end
