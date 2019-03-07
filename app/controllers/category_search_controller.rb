3b244985e68717c5a3d31c4190436150e17c756b
b-forster <brianna.forster@gmail.com>
2019-03-07 13:42:02 -0800

    Add search action to handle form submission (WIP)

--- app/controllers/category_search_controller.rb
+++ app/controllers/category_search_controller.rb
@@ -1,5 +1,11 @@
 class CategorySearchController < ApplicationController
   def index
+    render 'index'
+  end
 
+  def search
+    p "%" * 50
+    p params
+    p "%" * 50
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :search, controller: 'category_search', only: :create
+  # post 'search', to: 'category_search#search', as: 'search'
+
   root 'category_search#index'
 end
